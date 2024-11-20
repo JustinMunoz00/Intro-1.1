4fec5236f2857130564e3d2d1d9b890ae927dfd6
JustinMunoz00 <justinm880@nycstudents.net>
2024-11-20 16:22:52 +0000

    Fixed assignment 1.1

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Dear Mr. Holmer,");
+        System.out.println("I hope you had a good time with jury duty.");
+        System.out.println("I was hoping you could regrade this assignment and grade assignments 1.2 and 1.3 as well.");
+        System.out.println("Best Wishes,");
+        System.out.print("Justin Munoz");
+    }
+}
